fe46992bca4921109c16ea9ae990ab80b68615cc
Diego Arroyo Palacios <89891231+DiegoArroyo12@users.noreply.github.com>
2022-05-09 12:52:55 -0500

    JComboBox

--- sesiones/InterfacesUsuario2210/src/ico/fes/iu/swing/VentanaSwing.java
+++ sesiones/InterfacesUsuario2210/src/ico/fes/iu/swing/VentanaSwing.java
@@ -6,9 +6,14 @@ package ico.fes.iu.swing;
 
 import java.awt.FlowLayout;
 import java.awt.HeadlessException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 /**
@@ -18,6 +23,8 @@ import javax.swing.JTextField;
 public class VentanaSwing extends JFrame{
     private FlowLayout layout;
     private JTextField cuadroTexto;
+    private JButton boton;
+    private JLabel resultado;
 
     public VentanaSwing() throws HeadlessException {
         this.setTitle("Mi Ventana Swing");
@@ -26,8 +33,21 @@ public class VentanaSwing extends JFrame{
         layout = new FlowLayout();
         this.setLayout(layout);
         cuadroTexto = new JTextField(15);
+        boton = new JButton("Convertir a °F");
+        resultado = new JLabel("Grados °F");
+        JComboBox<String> lista = new JComboBox<String>();
+        lista.addItem("Rojo");
+        lista.addItem("Verde");
+        lista.addItem("Azul");
+        lista.addItem("Blanco");
+        
         this.getContentPane().add(cuadroTexto);
-        this.validate();
+        this.getContentPane().add(boton);
+        this.getContentPane().add(resultado);
+        this.getContentPane().add(lista);
+        this.pack();
+        this.setVisible(true);
+        //this.validate();
         
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -36,6 +56,19 @@ public class VentanaSwing extends JFrame{
                 //((JFrame)e.getSource()).setVisible(false);
             }
         });
+        
+        this.boton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Al siguiente le hará falta manejo de excepciones???
+                float centigrados = Float.parseFloat( cuadroTexto.getText() );
+                // °F = (°C x 9/5) + 32
+                float farenheit = (centigrados * (9.0f/5.0f)) + 32.0f;
+                resultado.setText(farenheit + " °F");
+            }
+            
+        });
+        
     }
     
     
